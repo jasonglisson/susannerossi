9db5b5b257f96fde0207dc8065684d4c6ec3bee2
jasonglisson <jason.glisson@gmail.com>
2016-05-05 20:46:57 -0400

    fix left margin

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'suzannerossi');
 
 /** MySQL database username */
-define('DB_USER', 'suzannerossi');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Admin2abc!');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
